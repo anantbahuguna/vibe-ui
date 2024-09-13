c03ff6f9e90e5d5e23f20e7539ab9ed96245cb06
anantbahuguna <anantbahugunaddn@gmail.com>
2024-09-13 20:19:38 +0530

    Add type declarations

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import tsConfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    dts({ rollupTypes: true, tsconfigPath: "./tsconfig.app.json" }),
+  ],
   build: {
     //library entry and output settings
     lib: {
